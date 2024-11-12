2c7e696b23f726a4f832ea9fc619049dd4bed850
Joathan Souza <joathanf@gmail.com>
2024-11-11 23:17:51 -0300

    Add methods to encode and decode JWT tokens in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
+# ApplicationController
 class ApplicationController < ActionController::API
+  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+
+  def encode_token(payload)
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def decode_token(token)
+    JWT.decode(token, SECRET_KEY)[0]
+  rescue StandardError
+    nil
+  end
 end

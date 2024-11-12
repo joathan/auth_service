1b500b093aa6a949866ab272fb8d71f798886d7f
Joathan Souza <joathanf@gmail.com>
2024-11-11 23:22:56 -0300

    Adds routes for registration and login to AuthController API v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      post 'register', to: 'auth#register'
+      post 'login', to: 'auth#login'
+    end
+  end
+
   get 'up' => 'rails/health#show', as: :rails_health_check
 end

f0bd83efd2d2ec2e1d8d7624cdb47f7126f9e027
Joathan Souza <joathanf@gmail.com>
2024-11-14 02:32:17 -0300

    Updates the JsonWebToken class to use the SECRET_KEY_BASE environment variable and specifies the HS256 algorithm for encoding and decoding JWT tokens.

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -3,17 +3,15 @@
 require 'jwt'
 
 class JsonWebToken
-  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+  SECRET_KEY = ENV['SECRET_KEY_BASE']
 
-  # Método para codificar o payload
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY)
+    JWT.encode(payload, SECRET_KEY, 'HS256')
   end
 
-  # Método para decodificar o token
   def self.decode(token)
-    decoded = JWT.decode(token, SECRET_KEY)[0]
+    decoded = JWT.decode(token, SECRET_KEY, true, { algorithm: 'HS256' })[0]
     HashWithIndifferentAccess.new(decoded)
   rescue JWT::ExpiredSignature
     Rails.logger.error 'Token has expired'

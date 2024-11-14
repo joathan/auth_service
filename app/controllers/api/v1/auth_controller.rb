0f3859198e192248d31fad46db1058b6c8fc4630
Joathan Souza <joathanf@gmail.com>
2024-11-14 02:36:01 -0300

    Add 'verify_token' method for validating JWT tokens in AuthController

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -16,17 +16,31 @@ module Api
       def login
         user = User.find_by(email: params[:email])
         if user&.authenticate(params[:password])
-          token = encode_token({ user_id: user.id })
+          token = JsonWebToken.encode(user_id: user.id)
           render json: { token: token }, status: :ok
         else
           render json: { error: 'Invalid email or password' }, status: :unauthorized
         end
       end
+
+      def verify_token
+        token = request.headers['Authorization']&.split(' ')&.last
+        user_id = JsonWebToken.decode(token)[:user_id]
+        user = User.find_by(id: user_id)
+        
+        if user
+          render json: { user_id: user.id }, status: :ok
+        else
+          render json: { error: 'Invalid token' }, status: :unauthorized
+        end
+      rescue JWT::DecodeError
+        render json: { error: 'Invalid token' }, status: :unauthorized
+      end
     
       private
     
       def user_params
-        params.permit(:email, :password)
+        params.require(:auth).permit(:email, :password)
       end
     end
   end
